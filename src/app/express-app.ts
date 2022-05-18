26589f6033706acefe5c137a506eb9a7d6fee9a1
Nikolas Furuta <nikolasfuruta@gmail.com>
2022-05-17 21:08:17 -0300

    feat: connect app to mongoDB

--- src/app/express-app.ts
+++ src/app/express-app.ts
@@ -1,14 +1,21 @@
 import express, { Express } from 'express'
 import cors from 'cors'
 import { router } from '../routes/paletas-route'
+import connectDB from '../db/prisma-connect'
 
 export class App {
   init: Express
+  db: Promise<void>
 
   constructor () {
     this.init = express()
+    this.db = this.conn()
     this.config()
     this.routes()
+  }
+
+  private async conn (): Promise<void> {
+    await connectDB()
   }
 
   private config (): void {
